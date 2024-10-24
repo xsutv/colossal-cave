858ed811e46b511585b79505baf01c2f02007d24
Haimen <haimenabbas@gmail.com>
2024-10-24 20:58:53 +0200

    Created GameApp as main-class and added CaveGame as a game class

---
+++ src/GameApp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import clazz.*;
+import enumz.*;
+
+public class GameApp {
+
+    @Deprecated
+    public static void main(String[] args) {
+
+        CaveGame game = new CaveGame();
+        
+        game.createPlayer();
+    }
+
+
+}
