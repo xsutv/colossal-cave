e079e12a0b392659c07548d039b81cf9e984fd98
Kod-Draken <m.dackhammar@gmail.com>
2024-10-24 15:06:09 +0200

    Main method here!

---
+++ CaveGame.java
@@ -0,0 +1,15 @@
+public class CaveGame {
+
+    @Deprecated
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cavern Explorer!");
+        System.out.println("jumping to method createPlayer()...");
+        System.out.println();
+    }
+
+    public static void createPlayer() {
+        System.out.println("Oops, no code here!");
+        System.out.println("Exiting...");
+        System.exit(0);
+    }
+}
