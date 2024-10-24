d683018de7e122b1beddbe1a9877aedaf7833adb
Haimen <haimenabbas@gmail.com>
2024-10-24 15:20:12 +0200

    updated structure

--- src/class/Player.java
+++ src/class/Player.java
@@ -1,7 +1,6 @@
 public class Player {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; ++i) {
-            System.out.println("Hej");
-        }
-    }
+
+    private String name;
+    private int hitPoints;
+    
 }
