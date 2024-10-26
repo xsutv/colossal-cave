de85c0b75f2c064e71206dba5a99f18df70ba9b0
Haimen <haimenabbas@gmail.com>
2024-10-26 15:10:19 +0200

    removed dublett

--- src/subclass/CaveGame.java
+++ src/subclass/CaveGame.java
@@ -11,10 +11,7 @@ public class CaveGame {
     List<Weapons> weps = new ArrayList<>(); // eller använda inbyggt enum array Weapons[]...
 
     // Default constructor for creating an instance of CaveGame.
-    public CaveGame() {
-
-
-    }
+    public CaveGame() {}
 
     // Should this method be in Player class?
     // Interactive creation of Player for a game.. should add try-catch handling
@@ -23,8 +20,6 @@ public class CaveGame {
 
             System.out.print("Choose a name for your character: ");
             String name = input.nextLine();
-            System.out.println("Choose a name for your character");
-            String name = input.nextLine().trim();
 
             System.out.print("Choose race of your character: ");
 
@@ -35,16 +30,7 @@ public class CaveGame {
                     System.out.print(Race.values()[i] + ", ");
                 }
             }
-
-            String playerRace = input.nextLine().toUpperCase();
-            for (int i = 0; i < Race.values().length; i++) {
-                if (i == Race.values().length - 1) {
-                    System.out.print(Race.values()[i] + ": ");
-                } else {
-                    System.out.print(Race.values()[i] + ", ");
-                }
-            }
-
+            
             String playerRace = input.nextLine().toUpperCase().trim();
 
             Player p = new Player(name, playerRace);
