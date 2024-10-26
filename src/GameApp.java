0f526e34fcfabbf6c21a8d8d23fc7765a9037dd3
Haimen <haimenabbas@gmail.com>
2024-10-26 16:45:54 +0200

    Moved createPlayer method from CaveGame to GameApp main.

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,16 +1,44 @@
 import java.util.*;
-
 import enums.*;
 import subclass.*;
+import subclass.items.*;
+import superclass.*;
 
 public class GameApp {
 
     public static void main(String[] args) {
 
         CaveGame game = new CaveGame();
-        
-        game.createPlayer();
-    }
 
+        // Interactive creation of Player for a game.. should add try-catch handling
+        try (Scanner input = new Scanner(System.in)) {
+
+            System.out.print("Choose a name for your character: ");
+            String name = input.nextLine();
+
+            System.out.print("Choose race of your character: ");
+
+            for (int i = 0; i < Race.values().length; i++) {
+                if (i == Race.values().length - 1) {
+                    System.out.print(Race.values()[i] + ": ");
+                } else {
+                    System.out.print(Race.values()[i] + ", ");
+                }
+            }
+            String playerRace = input.nextLine().toUpperCase().trim();
+
+            Player p = new Player(name, playerRace);
+
+            // Testing starts here::
+
+            System.out.println(p);
+
+            Apple apple = new Apple();
+
+            apple.interact(p);
+
+    
+        }
+    }
 
 }
