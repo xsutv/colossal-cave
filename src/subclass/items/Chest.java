a33548367dc9fc9ef73c8205e18db214094b45e9
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2024-10-26 15:55:19 +0200

    add methods inside Coins and Chest

--- src/subclass/items/Chest.java
+++ src/subclass/items/Chest.java
@@ -1,16 +1,23 @@
 package subclass.items;
 
+import java.util.*;
+
 import interfaces.*;
 
-public class Chest implements Interactive{
+public class Chest implements Interactive {
+    private List<Chest> chest = new ArrayList<>();
+
+    public void addItem(Chest item) {
+        chest.add(item);
+    }
 
     @Override
     public void interact() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'interact'");
     }
-    
+
 }
 
-
-// Add methods that gives multiple choice looting options. ( Dynamic method in Loot class).
+// Add methods that gives multiple choice looting options. ( Dynamic method in
+// Loot class).
--- src/subclass/items/Coins.java
+++ src/subclass/items/Coins.java
@@ -3,13 +3,18 @@ package subclass.items;
 import interfaces.*;
 
 public class Coins implements Interactive {
+    private int coinAmount;
+    private int coinSum;
+
+    public void addCoin(int coin) {
+        coinAmount += coin;
+        coinSum += coin;
+    }
 
     @Override
     public void interact() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'interact'");
     }
-    
-}
 
-// Add a method that adds coins to a looting bag.
+}
